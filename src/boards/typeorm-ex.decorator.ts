dc6b21c23aff8696471de892aee7550f84bfc8a2
LeeCh0129 <cksgh5477@gmail.com>
2023-03-28 01:01:14 +0900

    DB연동 및 게시물 생성/삭제, 커스텀 레포 타입수정

--- src/boards/typeorm-ex.decorator.ts
+++ src/boards/typeorm-ex.decorator.ts
@@ -1,7 +1,8 @@
 import { SetMetadata } from '@nestjs/common';
+import { ObjectType } from 'typeorm';
 
 export const TYPEORM_EX_CUSTOM_REPOSITORY = 'TYPEORM_EX_CUSTOM_REPOSITORY';
 
-export function CustomRepository(entity: Function): ClassDecorator {
+export function CustomRepository(entity: ObjectType<any>): ClassDecorator {
   return SetMetadata(TYPEORM_EX_CUSTOM_REPOSITORY, entity);
 }
