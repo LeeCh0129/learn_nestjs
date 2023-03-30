4e74c3e12f01455c971b0bf0a544307bc15b8ef7
LeeCh0129 <cksgh5477@gmail.com>
2023-03-31 02:42:36 +0900

    토큰 인증 후 유저정보 가져오기

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,20 +4,22 @@ import { PassportModule } from '@nestjs/passport';
 import { TypeOrmExModule } from 'src/boards/typeorm-ex.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { JwtStrategy } from './jwt.strategy';
 import { UserRepository } from './user.repository';
 
 @Module({
   imports: [
     PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
-      secret: 'Secret1234',
+      secret: 'Secret1234', // Secret Text
       signOptions: {
-        expiresIn: 60 * 60,
+        expiresIn: 60 * 60, // 토큰의 유효시간
       },
     }),
     TypeOrmExModule.forCustomRepository([UserRepository]),
   ],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, JwtStrategy], // 해당 부분은 AuthModule에서 사용하기 위함.
+  exports: [JwtStrategy, PassportModule], // 해당 부분은 다른 모듈에서도 사용하기 위함.
 })
 export class AuthModule {}
